0514a467c63f7570d3d91ca07cb4f7aca7c8bf35
henry <88551100+henrykgggg@users.noreply.github.com>
2022-10-07 14:42:08 -0700

    added clear function

---
+++ modules/clear.js
@@ -0,0 +1,16 @@
+
+const changeTodo = ({ index, status }) => {
+    Mylist[index - 1].completed = status;
+    localStorage.setItem('mylist', JSON.stringify(Mylist));
+    mylists();
+  };
+  const removeTodos = () => {
+    const uncompletedTodos = Mylist.filter((element) => element.completed !== true);
+    const newTodos = uncompletedTodos.map((element, index) => {
+      element.index = index + 1;
+      return element;
+    });
+    localStorage.setItem('mylist', JSON.stringify(newTodos));
+    updateTodo(newTodos);
+  };
+  
